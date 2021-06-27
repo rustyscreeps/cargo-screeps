b74fd23cf24ca42a10ada6903ac35bbb4d449630
Shane Madden <shanemadden@users.noreply.github.com>
2021-06-27 11:39:35 -0600

    Remove base64 hack and switch to newly-added binary buffer import, switch back to generating a module not the whole main.mjs

--- src/build/arena.rs
+++ src/build/arena.rs
@@ -94,20 +94,9 @@ pub fn build(root: &Path, build_config: &BuildConfiguration) -> Result<(), failu
     let generated_wasm = generated_wasm
         .ok_or_else(|| format_err!("error: no wasm files found in {}", target_dir.display()))?;
 
-    debug!("loading wasm file");
-    let generated_wasm_contents = fs::read(&generated_wasm)?;
-    let generated_wasm_b64 = base64::encode(&generated_wasm_contents);
+    debug!("renaming wasm file");
 
-    let folded_wasm_b64 = generated_wasm_b64.chars().enumerate()
-        .fold(String::new(), |acc, (i, c)| {
-            if i % 100 == 0 {
-                format!("{}' +\n'{}", acc, c)
-            } else {
-                format!("{}{}", acc, c)
-            }
-        });
-
-    let generated_wasm_rename_to = generated_wasm.with_extension("wasmorig");
+    let generated_wasm_rename_to = generated_wasm.with_extension("wasm.bin");
     fs::rename(&generated_wasm, &generated_wasm_rename_to)?;
 
     debug!("processing js file");
@@ -117,20 +106,18 @@ pub fn build(root: &Path, build_config: &BuildConfiguration) -> Result<(), failu
     let generated_js_rename_to = generated_js.with_extension("jsorig");
     fs::rename(&generated_js, &generated_js_rename_to)?;
 
-    let processed_js = process_js(&generated_js, &folded_wasm_b64, &generated_js_contents)?;
+    let processed_js = process_js(&generated_js, &generated_wasm_rename_to, &generated_js_contents)?;
 
-    let destination_mjs = generated_js.with_file_name("main.mjs");
+    debug!("writing processed js to {}", generated_js.display());
 
-    debug!("writing processed mjs with embedded wasm to {}", destination_mjs.display());
-
-    let mut output_handle = fs::File::create(destination_mjs)?;
+    let mut output_handle = fs::File::create(generated_js)?;
     output_handle.write_all(processed_js.as_bytes())?;
     output_handle.flush()?;
 
     Ok(())
 }
 
-fn process_js(file_name: &Path, wasm_data_b64: &str, input: &str) -> Result<String, failure::Error> {
+fn process_js(file_name: &Path, wasm_file_name: &Path, input: &str) -> Result<String, failure::Error> {
     // add polyfills for TextEncoder/TextDecoder as well as base64 conversion,
     // replace loader functions, and embed the base64-encoded wasm module in the mjs file
     let bindgen_output_regex = regex::Regex::new(&format!(
@@ -154,7 +141,6 @@ fn process_js(file_name: &Path, wasm_data_b64: &str, input: &str) -> Result<Stri
     })?;
 
     // CC-0 TextEncoder/TextDecoder polyfill from https://github.com/anonyco/FastestSmallestTextEncoderDecoder
-    // Apache 2.0 base64 encode/decode polyfill from https://github.com/davidchambers/Base64.js
     Ok(format!(
         r#"'use strict';(function(r){{function x(){{}}function y(){{}}var z=String.fromCharCode,v={{}}.toString,A=v.call(r.SharedArrayBuffer),B=v(),q=r.Uint8Array,t=q||Array,w=q?ArrayBuffer:t,C=w.isView||function(g){{return g&&"length"in g}},D=v.call(w.prototype);w=y.prototype;var E=r.TextEncoder,a=new (q?Uint16Array:t)(32);x.prototype.decode=function(g){{if(!C(g)){{var l=v.call(g);if(l!==D&&l!==A&&l!==B)throw TypeError("Failed to execute 'decode' on 'TextDecoder': The provided value is not of type '(ArrayBuffer or ArrayBufferView)'");
 g=q?new t(g):g||[]}}for(var f=l="",b=0,c=g.length|0,u=c-32|0,e,d,h=0,p=0,m,k=0,n=-1;b<c;){{for(e=b<=u?32:c-b|0;k<e;b=b+1|0,k=k+1|0){{d=g[b]&255;switch(d>>4){{case 15:m=g[b=b+1|0]&255;if(2!==m>>6||247<d){{b=b-1|0;break}}h=(d&7)<<6|m&63;p=5;d=256;case 14:m=g[b=b+1|0]&255,h<<=6,h|=(d&15)<<6|m&63,p=2===m>>6?p+4|0:24,d=d+256&768;case 13:case 12:m=g[b=b+1|0]&255,h<<=6,h|=(d&31)<<6|m&63,p=p+7|0,b<c&&2===m>>6&&h>>p&&1114112>h?(d=h,h=h-65536|0,0<=h&&(n=(h>>10)+55296|0,d=(h&1023)+56320|0,31>k?(a[k]=n,k=k+1|0,n=-1):
@@ -162,7 +148,8 @@ g=q?new t(g):g||[]}}for(var f=l="",b=0,c=g.length|0,u=c-32|0,e,d,h=0,p=0,m,k=0,n
 0,f=new t((l<<1)+8|0),b,c=0,u=!q;for(b=0;b<l;b=b+1|0,c=c+1|0){{var e=g.charCodeAt(b)|0;if(127>=e)f[c]=e;else{{if(2047>=e)f[c]=192|e>>6;else{{a:{{if(55296<=e)if(56319>=e){{var d=g.charCodeAt(b=b+1|0)|0;if(56320<=d&&57343>=d){{e=(e<<10)+d-56613888|0;if(65535<e){{f[c]=240|e>>18;f[c=c+1|0]=128|e>>12&63;f[c=c+1|0]=128|e>>6&63;f[c=c+1|0]=128|e&63;continue}}break a}}e=65533}}else 57343>=e&&(e=65533);!u&&b<<1<c&&b<<1<(c-7|0)&&(u=!0,d=new t(3*l),d.set(f),f=d)}}f[c]=224|e>>12;f[c=c+1|0]=128|e>>6&63}}f[c=c+1|0]=128|e&63}}}}return q?
 f.subarray(0,c):f.slice(0,c)}};E||(r.TextDecoder=x,r.TextEncoder=y)}})(""+void 0==typeof global?""+void 0==typeof self?this:self:global);
 
-!function(e){{"use strict";if("object"==typeof exports&&null!=exports&&"number"!=typeof exports.nodeType)module.exports=e();else if("function"==typeof define&&null!=define.amd)define([],e);else{{var t=e(),o=global;"function"!=typeof o.btoa&&(o.btoa=t.btoa),"function"!=typeof o.atob&&(o.atob=t.atob)}}}}(function(){{"use strict";var f="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";function c(e){{this.message=e}}return(c.prototype=new Error).name="InvalidCharacterError",{{btoa:function(e){{for(var t,o,r=String(e),n=0,a=f,i="";r.charAt(0|n)||(a="=",n%1);i+=a.charAt(63&t>>8-n%1*8)){{if(255<(o=r.charCodeAt(n+=.75)))throw new c("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");t=t<<8|o}}return i}},atob:function(e){{var t=String(e).replace(/[=]+$/,"");if(t.length%4==1)throw new c("'atob' failed: The string to be decoded is not correctly encoded.");for(var o,r,n=0,a=0,i="";r=t.charAt(a++);~r&&(o=n%4?64*o+r:r,n++%4)&&(i+=String.fromCharCode(255&o>>(-2*n&6))))r=f.indexOf(r);return i}}}}}});
+import wasm_bytes from "./{}";
+
 {}
 
 async function load(buffer, imports) {{
@@ -177,24 +164,8 @@ async function init() {{
     wasm = instance.exports;
 }}
 
-let wasm_b64 = '{}';
-
-let wasm_decoded = atob(wasm_b64);
-
-wasm_b64 = null;
-
-var len = wasm_decoded.length;
-var wasm_bytes = new Uint8Array(len);
-for (var i = 0; i < len; i++) {{
-    wasm_bytes[i] = wasm_decoded.charCodeAt(i);
-}}
-
-wasm_decoded = null;
-
 await init();
-
-wasm_bytes = null;
 "#,
-        &captures[1], &captures[2], wasm_data_b64
+        wasm_file_name.file_name().and_then(OsStr::to_str).unwrap_or(""), &captures[1], &captures[2]
     ))
 }
